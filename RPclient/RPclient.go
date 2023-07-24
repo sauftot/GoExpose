a2cf2dc998a14bcb875cf03fd383a202fb943c29
Antoine <97674510+sauftot@users.noreply.github.com>
2023-07-24 14:43:03 +0200

    progress RPclient

--- RPclient/RPclient.go
+++ RPclient/RPclient.go
@@ -1,8 +1,10 @@
 package rpclient
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"regexp"
 	"strings"
 )
 
@@ -10,6 +12,7 @@ var run bool = false
 var ctrlPort int = 47921
 var proxyPort int = 47922
 var RPserver string = ""
+var ip net.IP
 
 func isNumericalOnly(s string) bool {
 	for _, char := range s {
@@ -20,13 +23,39 @@ func isNumericalOnly(s string) bool {
 	return true
 }
 
-func consoleController(csl chan string) {
+func resolveDomain(domain string) (net.IP, error) {
+	ipRegex := `^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$`
+	if match, _ := regexp.MatchString(ipRegex, RPserver); match {
+		ip = net.ParseIP(RPserver)
+		if ip == nil {
+			fmt.Println("Invalid IP address.")
+			return nil, errors.New("invalid IP address")
+		}
+		return ip, nil
+	}
+
+	// Check if it's a valid domain name
+	domainRegex := `^([a-zA-Z0-9][a-zA-Z0-9-]*\.)+[a-zA-Z]{2,}$`
+	if match, _ := regexp.MatchString(domainRegex, RPserver); match {
+		ipAddr, err := net.ResolveIPAddr("ip", RPserver)
+		if err != nil {
+			fmt.Println("Error resolving domain:", err)
+			return nil, errors.New("error resolving domain")
+		}
+		ip = ipAddr.IP
+		fmt.Println("Resolved domain to IP:", ip.String())
+		return ip, nil
+	}
+	return nil, errors.New("invalid IP address or domain name")
+}
+
+func consoleController(csl chan bool) {
 	fmt.Println("Welcome to RPclient!")
 	var cslString string
 	for proceed := false; !proceed; {
 		fmt.Println("Please specify the address of your RPserver: ")
 		fmt.Scanln(&cslString)
-		// verify input, resolve domain, send address into csl
+		resolveDomain(cslString)
 	}
 	fmt.Println("Proceeding, exit the program with the command \"stop\"")
 	for {
@@ -52,9 +81,10 @@ func handlePair(locConn net.TCPConn, proxConn net.TCPConn) {
 
 func main() {
 	run = true
-	cslIn := make(chan string)
+	cslIn := make(chan bool)
 	go consoleController(cslIn)
-	RPserver = <-cslIn
+	<-cslIn
+
 	// convert RPserver string into net.IPAddr
 	// attempt connection and authentication on RPserver
 	// on-success: start relayPackets goroutine
